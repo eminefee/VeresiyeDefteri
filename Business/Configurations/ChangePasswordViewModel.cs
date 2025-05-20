35339f68f1d5405fbefaee81c23e0cc769a4ca05
stj01 <stj01@DESKTOP-IE25R8S>
2025-05-20 13:31:05 +0300

    password change

---
+++ Business/Configurations/ChangePasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Business.Configurations
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Şifre en az 6 karakter olmalı")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "Şifreler uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Business/Controllers/ProfileController.cs
@@ -0,0 +1,70 @@
+﻿using Business.Configurations;
+using Business.Context;
+using Business.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProfileController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProfileController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var user = _context.Userss.ToList();
+            return View(user);
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user = _context.Userss.FirstOrDefault(x => x.Id == id);
+            if (user == null) return NotFound();
+            _context.Userss.Remove(user);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = _context.Userss.FirstOrDefault(u => u.Username == User.Identity.Name);
+            if (user == null)
+                return NotFound();
+
+            var hasher = new PasswordHasher<User>();
+
+            // Şifre doğrulaması Microsoft'un hasher'ı ile yapılmalı
+            var result = hasher.VerifyHashedPassword(user, user.PasswordHash, model.OldPassword);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                ModelState.AddModelError("", "Eski şifre yanlış.");
+                return View(model);
+            }
+
+            // Yeni şifreyi hashle
+            user.PasswordHash = hasher.HashPassword(user, model.NewPassword);
+            _context.SaveChanges();
+
+            TempData["SuccessMessage"] = "Şifre başarıyla değiştirildi.";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
