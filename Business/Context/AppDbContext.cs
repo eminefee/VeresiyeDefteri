b0674f2028c7240190108fca1ff6c779286a2df2
emn0sk <efeemin935@gmail.com>
2025-05-26 19:00:33 +0300

    asdad

--- Business/Context/AppDbContext.cs
+++ Business/Context/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace Business.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<DebtRecord> debtrecords { get; set; }
+        public DbSet<DebtRecord> debtrecords { get; set; } 
         public DbSet<User> Userss { get; set; }
         public DbSet<SentEmail> SentEmails { get; set; }
         public DbSet<Product> Products { get; set; }
